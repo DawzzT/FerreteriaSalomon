de7fcbc07c3d423dfd2419609c6ffc384a10f355
Dawzz <85895419+DawzzT@users.noreply.github.com>
2022-11-04 11:28:38 -0600

    Add files via upload

---
+++ Ferreteria Salomon/Ferreteria Salomon/Conectar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferreteria_Salomon
+{
+ 
+    
+    internal class Conectar
+    {
+        public SqlConnection connect = new SqlConnection();
+
+        public Conectar(String user, String pass)
+        {
+            try
+            {
+
+                connect = new SqlConnection("Server=DESKTOP-9UHN9KS;Database=FerreteriaSalomon;UID=" + user + ";PWD=" + pass);
+                connect.Open();
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+    }
+}
