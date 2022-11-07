3e89f85a76367560602a222c08d484ddf7e7843e
MoisesNunez435 <85852193+MoisesNunez435@users.noreply.github.com>
2022-11-06 21:38:04 -0600

    Add files via upload

--- Sql/Procedimientos Proveedor, Compras.sql
+++ Sql/Procedimientos Proveedor, Compras.sql
@@ -132,3 +132,122 @@ end
 create procedure ListarP
 as
 select * from Proveedor where EstadoProv=1
+
+--Procedimientos para Compras
+
+--Incersion
+create procedure NCompra
+@IDP char(5)
+as 
+declare @idprov as int
+set @idprov=(select Id_Prov from Proveedor where Id_Prov=@IDP)
+if(@IDP='')
+begin 
+	print 'No puede ser nulo'
+end
+else
+begin 
+		if(@IDP=@idprov)
+		begin 
+			insert into Compras values(getdate(),@IDP,0)
+		end 
+		else
+		begin
+			print 'Proveedor no registrado'
+		end
+	end
+
+--Busqueda
+create procedure BCompra
+@IDC int
+as
+declare @idcomp as int
+set @idcomp=(select Id_Compra from Compras where Id_Compra=@IDC)
+if(@IDC = '')
+begin
+ print 'No puede ser nulo'
+end
+else
+begin
+if(@idcomp=@IDC)
+begin
+	select * from Compras where Id_Compra=@IDC
+end
+else
+begin
+  print 'Compra no encontrado'
+end
+end
+
+
+-- Lista
+create procedure ListarComp
+as
+select * from Compras 
+
+
+--Procedimientos para Det_Compras
+
+--Incersion
+create Procedure NDC
+@IDC int,
+@COP  char(5),
+@Cantc int,
+@PrecioC float
+as
+declare @idcomp as char(5)
+set @idcomp=(select Id_Compra from Compras where Id_Compra=@IDC)
+declare @codprod as char(5)
+set @codprod=(select CodProd from Productos where CodProd=@COP)
+if(@Cantc<=0 or @PrecioC<=0 )
+begin
+	print 'No pueden ser 0 o Menores'
+end 
+else
+begin
+	if(@idcomp=@IDC)
+	begin
+		insert into Det_Compras values (@IDC,@COP,@Cantc,@PrecioC,dbo.CSTC(@COP,@Cantc))
+	 end
+	 else
+	 begin 
+		print 'Compra no registrada'
+	 end
+end
+
+--Busqueda
+create procedure BuscarDetC
+@IDC int
+as
+declare @idcomp as int
+set @idcomp=(select Id_Compra from Compras where Id_Compra=@IDC)
+if(@idcomp=@IDC)
+begin
+	select * from Det_Compras where Id_Compra=@IDC
+end
+else
+begin
+  print 'Compra no encontrada'
+end
+
+--Lista
+create procedure ListarDetC
+as
+select * from Det_Compras
+
+create trigger ActInvC
+on Det_Compras
+after insert
+as
+update Productos set ExistP=ExistP+(select cantc from insertered)
+from Productos p, Det_Compras dc where p.CodProd=dc.CodProd
+
+create function CSTC(@COP int,@cc as int)
+returns float
+as
+begin
+  declare @stc as float
+  select @stc=PrecioP * @cc
+  from Productos where CodProd=@COP
+  return @stc
+end
